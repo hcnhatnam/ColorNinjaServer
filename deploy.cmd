162b51f5c207e8951734d2f9a474bf3b9d50589b
namhcn <nhatnam10a3@gmail.com>
2020-05-19 09:21:36 +0700

    change

--- deploy.cmd
+++
@@ -1,4 +0,0 @@
-mvn clean compile assembly:single
-sshpass -p "v7h3tVdpBtkbmkv" rsync -rauhv "$PWD" -e "ssh -i /home/namhcn/Documents/zalo/vngsave/key/keycmc" root@119.82.135.105:~/
-sshpass -p "v7h3tVdpBtkbmkv" ssh -i /home/namhcn/Documents/zalo/vngsave/key/keycmc root@119.82.135.105 sh /root/ColorNinjaServer/runservice
-
--- ssh.sh
+++
@@ -1,1 +0,0 @@
-ssh -i /home/namhcn/.ssh/id_vast -p 19000 root@ssh4.vast.ai
